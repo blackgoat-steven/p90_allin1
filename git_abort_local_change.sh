71b9bfc68229e8056e97e76debe37d2921f83352
blackgoat-steven <stevenlo@blackgoat.com.tw>
2023-01-11 14:26:08 +0800

    Update git_abort_local_change.sh

--- git_abort_local_change.sh
+++ git_abort_local_change.sh
@@ -3,3 +3,6 @@
 git fetch
 git reset --hard HEAD
 git merge '@{u}'
+
+
+chown -R librenms:librenms ./ ; chown apache:apache rrd ; chmod 777 -R bootstrap/cache storage logs rrd; chmod 755 *.php ; chmod 755 *.py ; chmod 755 cronic ;
